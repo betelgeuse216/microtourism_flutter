63eb84aea72cb6c38290ad89ba6ce0f8e7fd8b38
betelgeuse216 <betelgeuse216@gmail.com>
2020-11-09 23:03:42 +0900

    set class name esisting for test code build.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:guide_application/google_maps.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(MapSample());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
